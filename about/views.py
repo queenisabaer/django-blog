274cfa018566c6914df0fb0dd9eb2f327976f3d2
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-05-28 11:18:46 +0200

    Add collaboration form

--- about/views.py
+++ about/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 class AboutList(generic.ListView):
@@ -12,10 +14,23 @@ def about_starter(request):
     Renders the About page
     """
 
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working daysl'
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+        "collaborate_form": collaborate_form,},
     )
+
+    
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
+
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -26,12 +29,29 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        print("Received a POST request")
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
 
+    comment_form = CommentForm()
+    
+    print("About to render")
+    
     return render(
         request,
         "blog/post_detail.html",
         {"post": post,
          "comments": comments,
          "comment_count": comment_count,
+         "comment_form": comment_form
          },
     )
